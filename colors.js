baf28723833f45a02affea51bb098e32181b79e7
si3333 <151917068+si3333@users.noreply.github.com>
2023-12-07 21:51:17 +0900

    현재 페이지를 나타내요.
    window.location을 활용했음

--- colors.js
+++ colors.js
@@ -8,11 +8,16 @@ var Body = {
     }
 }
 var Links = {
-    setTextColor:function(color) {
-        var a_list = document.querySelectorAll('a');
+    setTextColor:function(currentPageLinkColor, color) {
+        var aList = document.querySelectorAll('a');
         var i = 0;
-        while (i < a_list.length) {
-            a_list[i].style.color = color;
+        while (i < aList.length) {
+            if (aList[i].href == window.location) {
+                aList[i].style.color = currentPageLinkColor;
+            }
+            else {
+                aList[i].style.color = color;
+            }
             i++;
         }
     }
@@ -22,15 +27,18 @@ function DayNightHandler(self) {
     if (self.value === 'night') {
         Body.setBackgroundColor('white');
         Body.setTextColor('black');
-        Links.setTextColor('blue');
+        Links.setTextColor('blue', 'gray');
         document.querySelector('ol').style.color = 'blue';
-        self.value = 'day';
+        self.value = 'day';   
     }
     else {
         Body.setBackgroundColor('black');
         Body.setTextColor('darkgray');
-        Links.setTextColor('cyan');
+        Links.setTextColor('green', 'cyan');
         document.querySelector('ol').style.color = 'cyan';
         self.value = 'night';
+        // 현재 페이지를 담고있는 링크의 색 = 진한 색
     }
 }
+// + 페이지가 새로고침 돼도, 버튼의 value값이 유지됐으면 좋겠어..
+// => window 객체, cookie에 대해 공부하기
